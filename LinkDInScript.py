e900a35d245ea8a31eabe5fd19fba99efe235fb7
ilyabidzep <ilya.kanterman@gmail.com>
2020-08-04 19:24:13 +0300

    Update LinkDInScript.py

--- LinkDInScript.py
+++ LinkDInScript.py
@@ -5,18 +5,20 @@ from time import sleep
 
 
 #change path to your local chromedriver.exe
-linkcon = webdriver.Chrome("D:\Your\Path\chromedriver.exe")
-linkcon.maximize_window()
 
+linkcon = webdriver.Chrome("C:\linkdinscript-master\chromedriver.exe")
+linkcon.maximize_window()
 
 username = input("Enter Your Email ")
 password = input("Enter Your Password ")
 
+
 linkcon.get("https://www.linkedin.com/")
 linkcon.find_element_by_xpath("//input[@name='session_key']").send_keys(username)
 linkcon.find_element_by_xpath("//input[@name='session_password']").send_keys(password)
-linkcon.find_element_by_id("login-submit").click()
-linkcon.find_element_by_xpath("//span[contains(.,'My Network')]").click()
+sleep(2)
+linkcon.find_element_by_xpath("//button[contains(.,'Sign in')]").click()
+linkcon.find_element_by_id("mynetwork-nav-item").click()
 
 sleep(4)
 body = linkcon.find_element_by_tag_name("body")
@@ -31,7 +33,4 @@ for x in cont:
     print('{}/{}'.format(x, len(cont)))
     sleep(2)
     x.click()
-
-
-
 
